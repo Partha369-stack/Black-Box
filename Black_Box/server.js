67ae85a7ccb528e63ee148c83bded5d0864d6f49
Partha <parthapradhan754@gmail.com>
2025-08-05 17:27:38 +0530

    Fix server binding and simplify Railway config for deployment

--- Black_Box/server.js
+++ Black_Box/server.js
@@ -16,6 +16,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Black Box Landing Page server running on port ${port}`);
 });
